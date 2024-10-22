d6430c4b448e94f6f40c4bc6756416d5b1927ad0
Keli06001 <keli06001@utb.vaxjo.se>
2024-10-22 15:37:55 +0200

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace inlämning_felhantering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett heltal");
+            string input=Console.ReadLine();
+            int result;
+            if (EgenTryParse(input, out result))
+            {
+                Console.WriteLine("Konverteringen lyckades: "+ result);
+            }
+            else
+            {
+                Console.WriteLine("Du angav något annat än ett heltal. (Avrättning)");
+            }
+
+        }
+        public static bool EgenTryParse(string input, out int result)
+        {
+            result = 0;
+
+           for(int i= 0; i<input.Length; i++)
+            {
+                if (input[i] <  '0' || input[i] >'9')
+                {
+                    return false;
+                }
+                result = result + input[i];
+            }
+           return true;
+        }
+    }
+}
